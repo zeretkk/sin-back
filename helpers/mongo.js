e51b20dbd86bd675efcdf4bbd43d5f18420f5fcf
zeret <kirill03192@gmail.com>
2023-02-13 17:54:12 +0300

    Fix unused import

--- helpers/mongo.js
+++ helpers/mongo.js
@@ -1,5 +1,4 @@
-const {MongoClient} = require("mongodb");
-const mongoClient = require('mongodb').MongoClient
+const {MongoClient} = require("mongodb")
 const client = new MongoClient('mongodb://127.0.0.1:27017')
 
 module.exports = client.db('sin')
